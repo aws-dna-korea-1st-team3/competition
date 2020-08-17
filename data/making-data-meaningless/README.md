894c943611cd16a854c94ca090ad68b73c1827e6
Myeongjae Kim <dev@myeongjae.kim>
2020-08-18 01:47:30 +0900

    [#5] process data according to format of AWS Personalize. (#6)

--- data/making-data-meaningless/README.md
+++ data/making-data-meaningless/README.md
@@ -3,19 +3,31 @@
 만화경 로그 데이터를 외부에 공개할 수 있도록 무의미하게 변경합니다.
 
 ## input
-`./input` directory 이하에 `.csv`파일들을 넣습니다. csv의 포맷은 다음과 같습니다.
+
+### 작품
+
+`./input/title/title.csv` 파일입니다. csv의 포맷은 다음과 같습니다.
 
 ```
-dvc_id,name,id,log_date
-bb50112c-5f41-49b6-a6ff-426b4b68c066,개구리공주,25,2020-05-31 15:00:58.751
-eff27a75-69de-445e-98cf-c3d9302369b3,나의 연애 D-day,37,2020-05-31 15:01:06.823
+ITEM_ID,CREATION_TIMESTAMP,TITLE
+"1","2019-08-16 04:48:10.032","개구리공주"
+"2","2019-08-16 04:50:12.047","하루의 끝을 당신과"
+```
+
+### 사용자-작품 조회 데이터
+
+`./input/title-read` directory 이하에 `.csv`파일들을 넣습니다. csv의 포맷은 다음과 같습니다.
+
+```
+"USER_ID","ITEM_ID","TIMESTAMP","TITLE","EVENT_TYPE"
+"38eb3ddb-3277-4ab9-b293-969c451314a5","4","2020-01-26 11:39:07.443","별일 없이 산다","title-read"
 ```
 
 ## output
-`./output` directory 이하에 .csv파일이 생성됩니다. dvc_id는 무작위로 생성된 uuid로 교체되고 id는 기존의 id값 범위 안에서 다른 값으로 변경됩니다.
+`./output` directory 이하에 .csv파일이 생성됩니다. USER_ID는 무작위로 생성된 uuid로 교체되고 작품의 ITEM_ID는 기존의 값 범위 안에서 다른 값으로 변경됩니다.
 
 ## run
 
 ```sh
-go run run.go title-read # title-read는 output파일의 prefix
+go run run.go
 ```
--- data/making-data-meaningless/run.go
+++ data/making-data-meaningless/run.go
@@ -5,7 +5,6 @@ import (
 	"encoding/csv"
 	"io"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"os"
 	"sort"
@@ -15,24 +14,83 @@ import (
 	"github.com/google/uuid"
 )
 
-type csvRow struct {
-	dvcID string
-	name  string
-	id    int
-	date  string
+type user struct {
+	id   string
+	name string
 }
 
-func (r csvRow) toStringSlice() []string {
-	return []string{r.dvcID, r.name, strconv.Itoa(r.id), r.date}
+func (u user) toStringArray() []string {
+	return []string{u.id, u.name}
 }
 
-func getCsvHead() []string {
-	return []string{"dvc_id", "name", "id", "log_date"}
+func getTitleReadHead() []string {
+	return []string{"USER_ID", "ITEM_ID", "TIMESTAMP", "TITLE", "EVENT_TYPE"}
 }
+
+func getUserHead() []string {
+	return []string{"USER_ID", "NAME"}
+}
+
+var dvcIDMapGlobal map[string]string = make(map[string]string)
+var outputDirectory = "output"
 
 func main() {
-	outputFileNamePrefix := os.Args[1]
-	inputDir := "input"
+	os.RemoveAll(outputDirectory)
+	os.Mkdir(outputDirectory, 0755)
+
+	idMap := processTitle()
+	userIDMap := processTitleRead(idMap)
+	processPseudoUserData(userIDMap)
+
+	return
+}
+
+func processTitle() map[string]string {
+	// Data format
+	// "ITEM_ID","CREATION_TIMESTAMP","TITLE"
+	// "1","2019-08-16 04:48:10.032","개구리공주"
+
+	rows := readCsvFile("input/title/title.csv")
+	head := rows[0]
+	data := rows[1:]
+
+	// shuffle id
+	ids := getColumn(data, 0)
+	shuffledIds := make([]string, len(ids))
+
+	copy(shuffledIds, ids)
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(shuffledIds), func(i, j int) { shuffledIds[i], shuffledIds[j] = shuffledIds[j], shuffledIds[i] })
+
+	idMap := make(map[string]string)
+
+	for i := 0; i < len(rows)-1; i++ {
+		idMap[ids[i]] = shuffledIds[i]
+	}
+
+	for _, r := range data {
+		r[0] = idMap[r[0]]
+	}
+
+	modifiedData := convertDateTimeColumnToUnixEpochTime(data, []int{1})
+	sort.Slice(modifiedData, func(i, j int) bool {
+		o1, err1 := strconv.Atoi(modifiedData[i][0])
+		check(err1)
+		o2, err2 := strconv.Atoi(modifiedData[j][0])
+		check(err2)
+
+		return o1 < o2
+	})
+
+	os.Mkdir(outputDirectory+"/title", 0755)
+	writeCsv(append([][]string{head}, modifiedData...), "output/title/title.csv")
+
+	return idMap
+}
+
+func processTitleRead(idMap map[string]string) []string {
+	inputDir := "input/title-read"
 	files, err := ioutil.ReadDir(inputDir)
 	check(err)
 
@@ -45,79 +103,75 @@ func main() {
 		csvPaths = append(csvPaths, inputDir+"/"+fileName)
 	}
 
-	outputDirectory := "output"
-	os.RemoveAll(outputDirectory)
-
-	rows := []csvRow{}
+	rows := [][]string{}
 	for _, path := range csvPaths {
-		rows = append(rows, getRows(path)...)
+		rows = append(rows, readCsvFile(path)[1:]...)
 	}
 
-	sort.Slice(rows, func(i, j int) bool { return rows[i].date < rows[j].date })
+	sort.Slice(rows, func(i, j int) bool { return rows[i][2] < rows[j][2] })
 
-	ids := []int{}
 	dvcIDs := []string{}
 
 	for _, r := range rows {
-		ids = append(ids, r.id)
-		dvcIDs = append(dvcIDs, r.dvcID)
+		dvcIDs = append(dvcIDs, r[0])
 	}
 
-	shuffledIds := make([]int, len(ids))
-
-	copy(shuffledIds, ids)
-
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(shuffledIds), func(i, j int) { shuffledIds[i], shuffledIds[j] = shuffledIds[j], shuffledIds[i] })
-
-	idMap := make(map[int]int)
 	dvcIDMap := make(map[string]string)
 
 	for i := 0; i < len(rows); i++ {
-		idMap[ids[i]] = shuffledIds[i]
 		newUUID, _ := uuid.NewRandom()
 		dvcIDMap[dvcIDs[i]] = newUUID.String()
 	}
 
 	for i := 0; i < len(rows); i++ {
-		rows[i].id = idMap[rows[i].id]
-		rows[i].dvcID = dvcIDMap[rows[i].dvcID]
+		rows[i][0] = dvcIDMap[rows[i][0]]
+		rows[i][1] = idMap[rows[i][1]]
 	}
 
 	rowsDividedByMonth := divideByMonth(rows)
 
-	os.Mkdir(outputDirectory, 0755)
 	for k, v := range rowsDividedByMonth {
-		file, err := os.Create(outputDirectory + "/" + outputFileNamePrefix + "-" + k + ".csv")
-		check(err)
-		defer file.Close()
+		os.Mkdir(outputDirectory+"/title-read", 0755)
+		dateTimeConverted := convertDateTimeColumnToUnixEpochTime(v, []int{2})
 
-		writer := csv.NewWriter(file)
-		defer writer.Flush()
-
-		writer.Write(getCsvHead())
-		for _, r := range v {
-			err := writer.Write(r.toStringSlice())
-			check(err)
-		}
+		writeCsv(append([][]string{getTitleReadHead()}, dateTimeConverted...), outputDirectory+"/title-read/"+"title-read-"+k+".csv")
 	}
+
+	userIDs := make([]string, 0, len(dvcIDMap))
+	for _, value := range dvcIDMap {
+		userIDs = append(userIDs, value)
+	}
+
+	return userIDs
 }
 
-func getRows(path string) []csvRow {
+func processPseudoUserData(userIDs []string) {
+	users := make([][]string, 0, len(userIDs))
+
+	for i, userID := range userIDs {
+		users = append(users, user{
+			id:   userID,
+			name: "username" + strconv.Itoa(i+1),
+		}.toStringArray())
+	}
+
+	rows := append([][]string{getUserHead()}, users...)
+	os.Mkdir(outputDirectory+"/user", 0755)
+	writeCsv(rows, outputDirectory+"/user/"+"user.csv")
+}
+
+func readCsvFile(path string) [][]string {
 	// Open the file
 	csvfile, err := os.Open(path)
-	if err != nil {
-		log.Fatalln("Couldn't open the csv file", err)
-	}
+	check(err)
+	defer csvfile.Close()
 
 	// Parse the file
 	r := csv.NewReader(bufio.NewReader(csvfile))
 	r.LazyQuotes = true
 
 	// Iterate through the records
-	rows := []csvRow{}
-
-	r.Read() // skip first row
+	rows := [][]string{}
 
 	for {
 		// Read each record from csv
@@ -126,31 +180,66 @@ func getRows(path string) []csvRow {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			check(err)
 		}
 
-		var row csvRow
-		row.dvcID = record[0]
-		row.name = record[1]
-		row.id, _ = strconv.Atoi(record[2])
-		row.date = record[3]
-
-		rows = append(rows, row)
+		rows = append(rows, record)
 	}
 
 	return rows
 }
 
-func divideByMonth(allRows []csvRow) map[string][]csvRow {
+func getColumn(rows [][]string, index int) []string {
+	column := []string{}
+
+	for _, row := range rows {
+		column = append(column, row[index])
+	}
+
+	return column
+}
+
+func convertToUnixEpochTime(datetime string) int64 {
+	time, err := time.Parse("2006-01-02 15:04:05.000", datetime)
+	check(err)
+
+	return time.Unix()
+}
+
+func convertDateTimeColumnToUnixEpochTime(rows [][]string, indicesOfDateTimeColumns []int) [][]string {
+	for _, r := range rows {
+		for _, i := range indicesOfDateTimeColumns {
+			r[i] = strconv.FormatInt(convertToUnixEpochTime(r[i]), 10)
+		}
+	}
+
+	return rows
+}
+
+func writeCsv(rows [][]string, path string) {
+	file, err := os.Create(path)
+	check(err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, r := range rows {
+		err := writer.Write(r)
+		check(err)
+	}
+}
+
+func divideByMonth(allRows [][]string) map[string][][]string {
 	toMonth := 7
-	m := make(map[string][]csvRow)
-	yearAndMonth := allRows[0].date[:toMonth]
+	m := make(map[string][][]string)
+	yearAndMonth := allRows[0][2][:toMonth]
 
 	for _, r := range allRows {
-		if yearAndMonth == r.date[:toMonth] {
+		if yearAndMonth == r[2][:toMonth] {
 			m[yearAndMonth] = append(m[yearAndMonth], r)
 		} else {
-			yearAndMonth = r.date[:toMonth]
+			yearAndMonth = r[2][:toMonth]
 		}
 	}
 
