9a7fe59dc691bf582bdf500454a40a94bd814fa7
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-08-22 01:39:21 +0900

    [#7] succeeded to import dataset.

--- recommendation-system/run.py
+++ recommendation-system/run.py
@@ -10,11 +10,18 @@ region='ap-northeast-2'
 
 personalize = boto3.client('personalize', region_name=region)
 s3 = boto3.client('s3', region_name=region)
+iam_client = boto3.client('iam', region_name=region)
+iam_resource = boto3.resource('iam', region_name=region)
 
 logging.basicConfig(level=logging.INFO)
 
-bucket_name = "team3-recommendation-system-data-7"
-data_directory = "data"
+BUCKET_NAME = "team3-recommendation-system-personalize-data-1"
+DATA_DIRECTORY = "data"
+
+TITLE = "title"
+USER = "user"
+TITLE_READ = "title-read"
+ROLE_NAME = "team3-recommendation-system"
 
 # utils
 def get_file_paths_recursively(dirname, ext):
@@ -40,26 +47,26 @@ def create_getting_next_dots():
 
 get_next_dots = create_getting_next_dots()
 
-def wait_until_status(lambda_to_get_status, message_prefix, expected_status):
+def wait_until_status(lambdaToGetStatus, messagePrefix, expectedStatus):
     while True:
-        status = lambda_to_get_status()
-        if status != expected_status:
-            logging.info(message_prefix + " current status: " + status + get_next_dots())
-            time.sleep(1)
+        status = lambdaToGetStatus()
+        if status != expectedStatus:
+            logging.info(messagePrefix + " current status: " + status + get_next_dots())
+            time.sleep(1.5)
         else:
             logging.info("expected status achieved: " + status)
             break
 
 # AWS
 def create_bucket():
-    logging.info('Create bucket. bucket_name: {}, region: {}'.format(bucket_name, region))
+    logging.info('Create bucket. bucket_name: {}, region: {}'.format(BUCKET_NAME, region))
 
     location = {'LocationConstraint': region}
-    s3.create_bucket(Bucket=bucket_name,
+    s3.create_bucket(Bucket=BUCKET_NAME,
                             CreateBucketConfiguration=location)
 
 def add_bucket_policy():
-    logging.info('Add bucket policy for AWS Personalize. bucket_name: {}'.format(bucket_name))
+    logging.info('Add bucket policy for AWS Personalize. bucket_name: {}'.format(BUCKET_NAME))
 
     policy = f'''{{
 "Version": "2012-10-17",
@@ -76,29 +83,29 @@ def add_bucket_policy():
             "s3:ListBucket"
         ],
         "Resource": [
-            "arn:aws:s3:::{bucket_name}",
-            "arn:aws:s3:::{bucket_name}/*"
+            "arn:aws:s3:::{BUCKET_NAME}",
+            "arn:aws:s3:::{BUCKET_NAME}/*"
         ]
     }}
 ]
 }}'''
-    bucket_policy = boto3.resource('s3').BucketPolicy(bucket_name)
+    bucket_policy = boto3.resource('s3').BucketPolicy(BUCKET_NAME)
     bucket_policy.put(
       ConfirmRemoveSelfBucketAccess=True,
       Policy=policy)
 
 
 def upload_data():
-    csv_file_paths = get_file_paths_recursively(data_directory, "csv")
+    csv_file_paths = get_file_paths_recursively(DATA_DIRECTORY, "csv")
     logging.info("Csv files to upload: {}".format(csv_file_paths))
 
     s3 = boto3.resource('s3')
     for p in csv_file_paths:
-        s3.meta.client.upload_file(p, bucket_name, p)
+        s3.meta.client.upload_file(p, BUCKET_NAME, p)
 
 
 def register_schema():
-    json_file_paths = get_file_paths_recursively(data_directory, "json")
+    json_file_paths = get_file_paths_recursively(DATA_DIRECTORY, "json")
 
     arn_dictionary = {}
 
@@ -128,13 +135,144 @@ def create_dataset_group():
 
     return dsg_arn
 
+def create_dataset(dataset_group_arn, schema_arn_dict):
+    dataset_arn = 'datasetArn'
+
+    title_dataset = personalize.create_dataset(
+      name = TITLE,
+      schemaArn = schema_arn_dict[TITLE],
+      datasetGroupArn = dataset_group_arn,
+      datasetType = "Items"
+    )
+    logging.info("titleDatasetArn: " + title_dataset[dataset_arn])
+
+    user_dataset = personalize.create_dataset(
+      name = USER,
+      schemaArn = schema_arn_dict[USER],
+      datasetGroupArn = dataset_group_arn,
+      datasetType = "Users"
+    )
+    logging.info("userDatasetArn: " + user_dataset[dataset_arn])
+
+    title_read_dataset = personalize.create_dataset(
+      name = TITLE_READ,
+      schemaArn = schema_arn_dict[TITLE_READ],
+      datasetGroupArn = dataset_group_arn,
+      datasetType = "Interactions"
+    )
+    logging.info("titleReadDatasetArn: " + title_read_dataset[dataset_arn])
+
+    dataset_arn_dict = {}
+    dataset_arn_dict[TITLE] = title_dataset[dataset_arn]
+    dataset_arn_dict[USER] = user_dataset[dataset_arn]
+    dataset_arn_dict[TITLE_READ] = title_read_dataset[dataset_arn]
+
+    return dataset_arn_dict
+
+def create_role():
+    logging.info("Creating a role for Personalize. Role name: " + ROLE_NAME)
+
+    iam_client.create_role(
+        RoleName = ROLE_NAME,
+        AssumeRolePolicyDocument = '''{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "personalize.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}''')
+
+    policy_name = 'Team3RecommendationSystemPersonalizeS3BucketAccessPolicy'
+    new_policy = iam_client.create_policy(
+        PolicyName=policy_name,
+        PolicyDocument=f'''{{
+    "Version": "2012-10-17",
+    "Id": "{policy_name}",
+    "Statement": [
+        {{
+            "Sid": "{policy_name}",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::{BUCKET_NAME}",
+                "arn:aws:s3:::{BUCKET_NAME}/*"
+            ]
+        }}
+    ]
+}}''')
+    new_policy_arn = new_policy["Policy"]["Arn"]
+    
+    policy = iam_resource.Policy(new_policy_arn)
+    policy.attach_role(RoleName=ROLE_NAME)
+
+    return new_policy_arn
+
+def import_dataset(dataset_arn_dict):
+    role_arn = iam_resource.Role(ROLE_NAME).arn
+    DATASET_IMPORT_JOB_ARN = 'datasetImportJobArn'
+
+    title_import_job = personalize.create_dataset_import_job(
+        jobName = 'TitleImportJob',
+        datasetArn = dataset_arn_dict[TITLE],
+        dataSource = {'dataLocation':f's3://{BUCKET_NAME}/data/title/title.csv'},
+        roleArn = role_arn)
+
+    user_import_job = personalize.create_dataset_import_job(
+        jobName = 'UserImportJob',
+        datasetArn = dataset_arn_dict[USER],
+        dataSource = {'dataLocation':f's3://{BUCKET_NAME}/data/user/user.csv'},
+        roleArn = role_arn) 
+
+    title_read_import_job = personalize.create_dataset_import_job(
+        jobName = 'TitleReadImportJob',
+        datasetArn = dataset_arn_dict[TITLE_READ],
+        dataSource = {'dataLocation':f's3://{BUCKET_NAME}/data/title-read/title-read.csv'},
+        roleArn = role_arn) 
+
+    wait_until_status(
+        lambdaToGetStatus = lambda _="" : personalize.describe_dataset_import_job(
+            datasetImportJobArn=title_import_job[DATASET_IMPORT_JOB_ARN])["datasetImportJob"]["status"],
+        messagePrefix = "Importing title dataset group...",
+        expectedStatus = "ACTIVE")
+
+    wait_until_status(
+        lambdaToGetStatus = lambda _="" : personalize.describe_dataset_import_job(
+            datasetImportJobArn=user_import_job[DATASET_IMPORT_JOB_ARN])["datasetImportJob"]["status"],
+        messagePrefix = "Importing user dataset group...",
+        expectedStatus = "ACTIVE")
+
+    wait_until_status(
+        lambdaToGetStatus = lambda _="" : personalize.describe_dataset_import_job(
+            datasetImportJobArn=title_read_import_job[DATASET_IMPORT_JOB_ARN])["datasetImportJob"]["status"],
+        messagePrefix = "Importing title-read dataset group...",
+        expectedStatus = "ACTIVE")
+
+
+def delete_role(policyArn):
+    logging.info("Delete role. Role name: " + ROLE_NAME)
+    iam_client.detach_role_policy(
+      RoleName=ROLE_NAME,
+      PolicyArn=policyArn
+    )
+
+    iam_client.delete_role(RoleName=ROLE_NAME)
+    iam_client.delete_policy(PolicyArn=policyArn)
+
 def delete_schema(arn):
     personalize.delete_schema(schemaArn=arn)
 
 def delete_schemas(arns):
     logging.info("Delete schema: " + ", ".join(arns))
     for arn in arns:
-      delete_schema(arn)
+        delete_schema(arn)
 
 def delete_dataset_group(dsg_arn):
     personalize.delete_dataset_group(datasetGroupArn=dsg_arn)
@@ -148,31 +286,57 @@ def delete_dataset_group(dsg_arn):
     except:
         logging.info("Dataset group has been deleted.")
 
-def destroy():
-    logging.info('Destory all resources: {}'.format(bucket_name))
+def delete_dataset(arns):
+    logging.info("Delete dataset: " + ", ".join(arns))
+    for arn in arns:
+        personalize.delete_dataset(datasetArn=arn)
+
+    logging.info("Wait until datasets are deleted...")
+    time.sleep(10)
+
+
+def delete_bucket():
+    logging.info('Destory all resources: {}'.format(BUCKET_NAME))
     # TODO: delete all s3 files of the bucket.
     # TODO: delete s3 bucket.
     # TODO: delete dataset group
     # TODO: delete schema
 
     s3_client = boto3.client('s3')
-    s3_client.delete_bucket(Bucket=bucket_name)
+    s3_client.delete_bucket(Bucket=BUCKET_NAME)
 
 
 ##### main
 create_bucket()
 add_bucket_policy()
-# upload_data()
+upload_data()
 
-schema_arns = register_schema()
-# schema_arns = {'title': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/title', 'title-read': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/title-read', 'user': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/user'}
-# logging.info(schema_arns)
+policy_arn = create_role()
+
+schema_arn_dict = register_schema()
+# schema_arn_dict = {'title': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/title', 'title-read': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/title-read', 'user': 'arn:aws:personalize:ap-northeast-2:772278550552:schema/user'}
 
 dsg_arn = create_dataset_group()
 # dsg_arn = 'arn:aws:personalize:ap-northeast-2:772278550552:dataset-group/manhwakyung-title-recommendation'
 
+dataset_arn_dict = create_dataset(dsg_arn, schema_arn_dict)
+# dataset_arn_dict = {}
+# dataset_arn_dict[TITLE] = 'arn:aws:personalize:ap-northeast-2:772278550552:dataset/manhwakyung-title-recommendation/ITEMS'
+# dataset_arn_dict[USER] = 'arn:aws:personalize:ap-northeast-2:772278550552:dataset/manhwakyung-title-recommendation/USERS'
+# dataset_arn_dict[TITLE_READ] = 'arn:aws:personalize:ap-northeast-2:772278550552:dataset/manhwakyung-title-recommendation/INTERACTIONS'
+
+import_dataset(dataset_arn_dict)
+
+exit(0)
+
+# TODO: add separated script to delete resources.
+
+delete_dataset(dataset_arn_dict.values())
 delete_dataset_group(dsg_arn) 
-delete_schemas(schema_arns.values())
+delete_schemas(schema_arn_dict.values())
+
+#policy_arn='arn:aws:iam::772278550552:policy/Team3RecommendationSystemPersonalizeS3BucketAccessPolicy'
+delete_role(policy_arn)
 
 # TODO: delete all data in s3?
-destroy()
+delete_bucket()
