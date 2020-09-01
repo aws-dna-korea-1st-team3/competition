7bdbac5e221e0f447ab4c614d8f03c03266d8ef1
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-09-01 23:09:09 +0900

    [#14] add font.

--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -19,7 +19,7 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>만화경 작품 추천 시스템 :: AWS DNA</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
