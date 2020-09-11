c354262ce22cf4c0b8b2435fa4a783da8f7726ce
sangyun-han <sangyun628@gmail.com>
2020-09-12 01:55:25 +0900

    add static table and lambda code

--- preprocessing/preprocessing/preprocessing_stack.py
+++ preprocessing/preprocessing/preprocessing_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_glue as glue,
     aws_s3_deployment as s3_deployment,
     aws_iam as iam,
+    aws_lambda as _lambda,
     core
 )
 import boto3
@@ -17,7 +18,7 @@ SAMPLE_DATA_DIRECTORY = "data"
 TITLE = "title"
 USER = "user"
 TITLE_READ = "title-read"
-GLUE_DATABASE_NAME = "dna-database"
+GLUE_DATABASE_NAME = "dna_database"
 CRAWLER_SCHEDULE = "cron(0/5 * * * ? *)"
 
 
@@ -57,53 +58,99 @@ class PreprocessingStack(core.Stack):
             managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSGlueServiceRole')]
         )
 
-        glue.Database(
+        dna_database = glue.Database(
             self, "dna-glue-database-id", database_name=GLUE_DATABASE_NAME
         )
 
 
-        title_ad_crawler = glue.CfnCrawler(
+        glue.CfnCrawler(
             self, '{}-crawler-id'.format(TITLE_READ),
             description="Glue Crawler for {}".format(TITLE_READ),
             name='{}-crawler'.format(TITLE_READ),
             database_name=GLUE_DATABASE_NAME,
-            schedule={"scheduleExpression": CRAWLER_SCHEDULE},
+            # schedule={"scheduleExpression": CRAWLER_SCHEDULE},
             role=glue_role.role_arn,
             targets={"s3Targets": [{"path": "s3://{}/{}/{}.csv".format(ORIGINAL_DATA_BUCKET_NAME, TITLE_READ, TITLE_READ)}]}
         )
 
-        title_crawler = glue.CfnCrawler(
+        glue.CfnCrawler(
             self, '{}-crawler-id'.format(TITLE),
             description="Glue Crawler for {}".format(TITLE),
             name='{}-crawler'.format(TITLE),
             database_name=GLUE_DATABASE_NAME,
-            schedule={"scheduleExpression": CRAWLER_SCHEDULE},
+            # schedule={"scheduleExpression": CRAWLER_SCHEDULE},
             role=glue_role.role_arn,
             targets={"s3Targets": [{"path": "s3://{}/{}/{}.csv".format(ORIGINAL_DATA_BUCKET_NAME, TITLE, TITLE)}]}
         )
 
-        user_crawler = glue.CfnCrawler(
+        glue.CfnCrawler(
             self, '{}-crawler-id'.format(USER),
             description="Glue Crawler for {}".format(USER),
             name='{}-crawler'.format(USER),
             database_name=GLUE_DATABASE_NAME,
-            schedule={"scheduleExpression": CRAWLER_SCHEDULE},
+            # schedule={"scheduleExpression": CRAWLER_SCHEDULE},
             role=glue_role.role_arn,
             targets={"s3Targets": [{"path": "s3://{}/{}/{}.csv".format(ORIGINAL_DATA_BUCKET_NAME, USER, USER)}]}
         )
 
-        # glue.CfnJob(
-        #     self, "{}-job-id".format(TITLE_READ),
-        #     name="{}-job".format(TITLE_READ),
-        #     command={
-        #         "name": "glueetl",
-        #         "python_version": "3",
-        #         "script_location": "s3://{}/glue/spark/__main__.py".format(ORIGINAL_DATA_BUCKET_NAME)
-        #     },
-        #     role=glue_role.role_arn,
-        #     worker_type="Standard",
-        #
-        # )
-        
+        # create glue table
+        title_read_table = glue.Table(
+            self, "{}-table-id".format(TITLE_READ),
+            table_name="{}_table".format(TITLE_READ).replace("-", "_"),
+            database=dna_database,
+            columns=[
+                {"name": "USER_ID", "type": glue.Schema.STRING},
+                {"name": "ITEM_ID", "type": glue.Schema.STRING},
+                {"name": "TIMESTAMP", "type": glue.Schema.BIG_INT},
+                {"name": "TITLE", "type": glue.Schema.STRING},
+                {"name": "EVENT_TYPE", "type": glue.Schema.STRING}
+            ],
+            data_format=glue.DataFormat.CSV,
+            bucket=s3_org_data,
+            s3_prefix=TITLE_READ
+        )
 
-    
+        title_table = glue.Table(
+            self, "{}-table-id".format(TITLE),
+            table_name="{}_table".format(TITLE).replace("-", "_"),
+            database=dna_database,
+            columns=[
+                {"name": "ITEM_ID", "type": glue.Schema.STRING},
+                {"name": "CREATION_TIMESTAMP", "type": glue.Schema.BIG_INT},
+                {"name": "TITLE", "type": glue.Schema.STRING},
+                {"name": "TAG", "type": glue.Schema.STRING}
+            ],
+            data_format=glue.DataFormat.CSV,
+            bucket=s3_org_data,
+            s3_prefix=TITLE
+        )
+
+        user_table = glue.Table(
+            self, "{}-table-id".format(USER),
+            table_name="{}_table".format(USER).replace("-", "_"),
+            database=dna_database,
+            columns=[
+                {"name": "USER_ID", "type": glue.Schema.STRING},
+                {"name": "NAME", "type": glue.Schema.STRING},
+                {"name": "EMAIL", "type": glue.Schema.STRING},
+                {"name": "GENDER", "type": glue.Schema.STRING, "categorical": True},
+                {"name": "AGE", "type": glue.Schema.BIG_INT, "categorical": True},
+            ],
+            data_format=glue.DataFormat.CSV,
+            bucket=s3_org_data,
+            s3_prefix=USER
+        )
+
+        ctas_lambda_func = _lambda.Function(
+            self, "CTAS_query",
+            function_name="CTAS_query",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset("src/lambda"),
+            handler="ctas_lambda.lambda_handler",
+            description="CTAS query to transform AVRO file, batch execution",
+            environment={
+                "BUCKET_NAME": s3_transformed_data.bucket_name,
+                "DATABASE_NAME": GLUE_DATABASE_NAME
+            },
+            timeout=core.Duration.minutes(3)
+        )
