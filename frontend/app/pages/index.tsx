9e1c5967559084ae0467030af9b3dae7f113befe
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-09-25 22:54:05 +0900

    update frontend main text.

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,4 +1,4 @@
-import { createStyles, makeStyles } from '@material-ui/core';
+import { createStyles, Link, makeStyles } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
@@ -45,16 +45,18 @@ export default function Index() {
         <div className={clsx(classes.nonSelectable, classes.textContainer)}>
           <Typography className={classes.subHeader} variant="h4">
             AWS DNA 1st :: Team 3
-        </Typography>
+          </Typography>
           <Typography className={classes.mainHeader} variant="h1" component="h1" gutterBottom>
             만화경 작품 추천 시스템
-        </Typography>
+          </Typography>
           <Typography>
-            뭔가 허전해서 넣은 글씨.. 시스템에 대한 설명, 기여한 사람 등의 정보를 넣으면 될 것 같다.
-        </Typography>
+            AWS Personalize로 유사 작품 추천 및 개인화 추천을 합니다.
+          </Typography>
           <Typography>
-            여러 줄로 나눠서
-        </Typography>
+            <Link href="https://github.com/aws-dna-korea-1st-team3/competition" target="_blank">
+              https://github.com/aws-dna-korea-1st-team3/competition
+            </Link>
+          </Typography>
         </div>
         <TitleListContainer />
         <Copyright />
